fe79a5ca4a8c64bd4928736d553629f700b63f47
srjdat <srjdatta7@gmail.com>
2026-09-07 22:50:52 -0500

    documentation and path checking

--- src/image_loader.py
+++ src/image_loader.py
@@ -2,6 +2,10 @@ from PIL import Image
 import os
 from dotenv import load_dotenv
 
+"""
+    Loads images from given file path in the .env file. 
+    Checks if path exists, if so go through and get all the images. 
+"""
 def load_images(): 
     
     images = [] # where all the images will go
@@ -11,16 +15,20 @@ def load_images():
     load_dotenv()
     path = os.getenv('FILE_PATH') # open all images in this folder 
     num = 0 # naming scheme
-    for i in os.scandir(path=path): 
-        if i.name.lower().endswith(VALID_FORMATS): # make sure it's actually an image
-            img = Image.open(i)
-            images.append(img) 
+    if path: 
+        for i in os.scandir(path=path): 
+            if i.name.lower().endswith(VALID_FORMATS): # make sure it's actually an image
+                img = Image.open(i)
+                images.append(img) 
 
     print(len(images))
-    
+
     return images
+
+
 def main(): 
     load_images()
+
 
 if __name__ == "__main__":
     main()
